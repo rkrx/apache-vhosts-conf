a4f001a5d0222590347ed708c8981dd1511375a8
ron <ron.kirschler@gmail.com>
2013-10-12 23:17:40 +0200

    Initial checkin

---
+++ vhConf3/Property.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace vhConf3 {
+	public class Property {
+		public String name = "";
+		public String value = "";
+	}
+}
